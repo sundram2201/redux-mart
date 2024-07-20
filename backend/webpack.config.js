f59e5071321ad003176b9d31c29a0228e81e2300
Sundram Chauhan <sndrmchauhan@gmail.com>
2024-07-20 12:47:13 +0530

    webpack update

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -2,13 +2,14 @@ const path = require("path");
 const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
-  entry: "./index.js",
+  entry: "./index.js", // Use index.js as the entry point
   target: "node",
   externals: [nodeExternals()],
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
+  mode: "production", // Set the mode to 'production'
   module: {
     rules: [
       {
