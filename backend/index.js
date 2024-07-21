d2ed45cd354192c289ff167de83edf72088b2dc1
Sundram Chauhan <sndrmchauhan@gmail.com>
2024-07-21 11:26:52 +0530

    502

--- backend/index.js
+++ backend/index.js
@@ -22,6 +22,10 @@ app.options(
 );
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(`Received ${req.method} request for ${req.url}`);
+  next();
+});
 //routes
 
 const UserRoute = require("./routes/user");
@@ -31,6 +35,12 @@ app.use("/api/v1/user", UserRoute);
 app.use("/api/v1/product", ProductRoute);
 
 app.get("/", (_, res) => res.send(`Welcom to port : ${port}`));
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
 app.listen(port, () => console.log(`Server @${port}`));
 
 // DB Connectivity
